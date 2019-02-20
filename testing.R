c0958e4ac3b25d53c8dd1517bbdaec5df35c924c
WeiquanLuo <weiquan.luo1221@gmail.com>
2019-02-20 16:34:24 -0600

    update for basic map

---
+++ testing.R
@@ -0,0 +1,13 @@
+library(ggplot2)
+library(sf)
+
+
+p <- ggplot() +
+  geom_sf(data = read_sf("data/ME-GIS/Coastline2.shp"), 
+          colour="grey10", fill="grey90") +
+  geom_sf(data = read_sf("data/ME-GIS/Rivers19.shp"), 
+          colour="steelblue", size=0.3) +
+  geom_sf(data = read_sf("data/ME-GIS/PrimaryRoads.shp"), 
+          size = 0.7, colour="grey30") +
+  geom_sf(data = read_sf("data/ME-GIS/Cities.shp")) +
+  theme_bw()
